4675d6ff03cedca51ce268808ee2a612e081cbf6
Dullayut <theadfine@gmail.com>
2020-08-27 00:54:47 +0700

    replace observer class

---
+++ src/observer/ObserverApi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer;
+
+/**
+ *
+ * @author thead
+ */
+public class ObserverApi{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Senior obj1 = new Senior();
+	Marketing obj2 = new Marketing();
+	HeadQuater observable = new HeadQuater();              
+	observable.addObserver(obj1);              
+	observable.addObserver(obj2);              
+	observable.setSomeData(6);
+        observable.setSomeData(7);
+    }
+
+}
